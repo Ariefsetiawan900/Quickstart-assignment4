f157da3dcd5c2e4b430b97347bb48d51ddc532df
Ariefsetiawan900 <arief.setiawan8788@gmail.com>
2020-02-24 21:39:22 +0700

    update file 1.js

--- 1.js
+++ 1.js
@@ -1,26 +1,26 @@
-function operasi(angka){
+function operation(number){
     let sum = 0;
-for (let i = angka.length-1; i>=0 ; i--) {  //looping dari index terakhir
-   sum += angka [i]  
+for (let i = number.length-1; i>=0 ; i--) {  //looping dari index terakhir
+   sum += number [i]  
     
 }
 return sum
 }
-console.log(operasi([123, 9, 27])); // 159
-console.log(operasi([1, 5, 9])); //15
-console.log(operasi([1, 5])); //6
+console.log(operation([123, 9, 27])); // 159
+console.log(operation([1, 5, 9])); //15
+console.log(operation([1, 5])); //6
 
 
-function operasi1(angka){
-    let sum = 0;
-for (let i = 0; i<angka.length ; i++) { // looping index ke nol
-   sum += angka [i]  
+// function operasi1(angka){
+//     let sum = 0;
+// for (let i = 0; i<angka.length ; i++) {  looping index ke nol
+//    sum += angka [i]  
     
-}
-return sum
-}
-console.log(operasi1([123, 9, 27])); // 159
-console.log(operasi1([1, 5, 9])); //15
-console.log(operasi1([1, 5])); //6
+// }
+// return sum
+// }
+// console.log(operasi1([123, 9, 27])); // 159
+// console.log(operasi1([1, 5, 9])); //15
+// console.log(operasi1([1, 5])); //6
 
 
